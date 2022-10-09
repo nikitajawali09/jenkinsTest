f27b77a92ec1f740aa9cc0684208bd25226a343d
Nikita Jawali <nikitajawali06@gmail.com>
2022-10-09 21:03:03 +0530

    update

--- src/main/java/com/example/demo/Impl/Test.java
+++ src/main/java/com/example/demo/Impl/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public void print() {
 		System.out.println("Hi everyone...........");
-		System.out.println("Jenkins");
+		System.out.println("Jenkins"
+				+ "");
 		System.out.println("New");
 		
 		
