57316a61d61a7f23c93e2dc772741d342dcd4ea9
Nikita Jawali <nikitajawali06@gmail.com>
2022-10-22 12:36:37 +0530

    test case added

---
+++ src/main/java/com/example/demo/Impl/Calculator.java
@@ -0,0 +1,15 @@
+package com.example.demo.Impl;
+
+public class Calculator {
+
+
+	// Method to add 2 numbers
+	public int addNumbers(int numberOne, int numberTwo) {
+		return numberOne + numberTwo;
+	}
+	// Method to subtract 2 numbers
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		return numberOne - numberTwo;
+	}
+	
+}
---
+++ src/test/java/com/example/demo/Impl/CalcTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.Impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.beans.factory.annotation.Autowired;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CalcTest {
+	
+	@Mock
+	Calculator Calculator;
+
+	@Test
+	public void addTest() {
+		Calculator myCalc = new Calculator();
+		
+		Mockito.when(Calculator.addNumbers(10, 6));
+		Assert.assertNotNull(myCalc);
+	}
+
+	
+}
